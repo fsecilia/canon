0d181c821eae8d12be58989ef371abd670d6a9b8
Frank Secilia <frank.secilia@gmail.com>
2026-09-23 19:28:43 -0400

    feat: add canon_apply_target()

---
+++ cmake/Canon.cmake
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2026 Frank Secilia
+
+include_guard(GLOBAL)
+
+# Applies Canon's private build policy to a target that compiles C++ sources.
+function(canon_apply_target TARGET)
+    if (NOT TARGET "${TARGET}")
+        message(FATAL_ERROR "canon_apply_target(): target '${TARGET}' does not exist")
+    endif()
+
+    get_target_property(_type "${TARGET}" TYPE)
+    if (NOT _type STREQUAL "EXECUTABLE"
+        AND NOT _type STREQUAL "STATIC_LIBRARY"
+        AND NOT _type STREQUAL "SHARED_LIBRARY"
+        AND NOT _type STREQUAL "MODULE_LIBRARY"
+        AND NOT _type STREQUAL "OBJECT_LIBRARY")
+        message(FATAL_ERROR
+            "canon_apply_target(): target '${TARGET}' has type '${_type}', which has no compiled-target Canon policy")
+    endif()
+endfunction()
